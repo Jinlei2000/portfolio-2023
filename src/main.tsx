69120407a1551052e44b5efd538c626ed1156418
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-03-13 12:17:41 +0100

    feat: intalling vite, tailwindcss, routes

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './assets/tailwindcss.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className='text-3xl'>Hello World</div>
+  </React.StrictMode>,
+)
