c7cdc85385ee2669b29f356660ed1622d94ef362
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-04-13 21:16:39 +0200

    feat: intall fonts

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,8 @@
 export default () => {
   return (
     <div>
-      <h1 className="text-2xl font-medium">Home</h1>
-      <h1>Home</h1>
+      <h1 className="text-2xl font-medium">home iiii</h1>
+      <h1>home</h1>
     </div>
   )
 }
