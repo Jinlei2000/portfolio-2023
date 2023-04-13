d351f706ab646febbcd0b3d6bbc9e6fa01d29efc
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-04-13 20:46:36 +0200

    feat: add fonts

---
+++ src/routes/Home.tsx
@@ -0,0 +1,8 @@
+export default () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-medium">Home</h1>
+      <h1>Home</h1>
+    </div>
+  )
+}
---
+++ src/routes/projects/InteractivePoles.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div>
+      <h1>InteractivePoles</h1>
+    </div>
+  )
+}
