967701eb3ec3a6e2152b715625f64d62edf18ae1
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-04-20 23:26:10 +0200

    feat: projects section completed

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { motion } from 'framer-motion'
 import Typewriter from '../components/Typewriter'
@@ -12,6 +12,13 @@ import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide'
 import { projectsData } from '../data/projectsData'
 import FeaturedProjects from '../components/FeaturedProjects'
 import { Link } from 'react-router-dom'
+import {
+  ChevronLeft,
+  ChevronLeftIcon,
+  ChevronRight,
+  ChevronRightIcon,
+} from 'lucide-react'
+import CarouselProjects from '../components/CarouselProjects'
 
 export default () => {
   const [selectedSection, setSelectedSection] = useState('about')
@@ -125,64 +132,12 @@ export default () => {
               <h3 className="text-xl font-semibold text-own-neutral-900 dark:text-own-neutral-200 md:text-2xl xl:text-3xl">
                 More of what I've done
               </h3>
-              <Splide
-                options={{
-                  type: 'loop',
-                  perPage: 3,
-                  focus: 'center',
-                  perMove: 1,
-                  // autoplay: true,
-                  // interval: 8000,
-                  // flickMaxPages: 3,
-                  pagination: false,
-                  throttle: 300,
-                  wheel: true,
-                  gap: '3rem',
-                  updateOnMove: true,
-                  breakpoints: {
-                    768: {
-                      perPage: 1,
-                    },
-                  },
-                }}
-              >
-                {/* use map to render the rest of the projects */}
-                {projectsData.map((project, index) => {
-                  return (
-                    <SplideSlide key={index} className="splide__slide-carousel">
-                      <Link
-                        to={project.link}
-                        className="group/card focus:outline-none"
-                      >
-                        <div className="flex flex-col justify-center gap-2 rounded-lg group-focus-visible/card:border-2 group-focus-visible/card:border-opacity-100">
-                          <div className="group/img relative overflow-hidden rounded-lg">
-                            <img
-                              src={project.image}
-                              alt={project.name}
-                              className="h-full w-full object-cover transition duration-500 ease-in-out group-hover/img:rotate-1 group-hover/img:scale-105"
-                            />
-                            <div className="absolute inset-0 bg-own-neutral-900 bg-opacity-70 opacity-0 transition duration-500 ease-in-out group-hover/img:opacity-100">
-                              <div className="flex h-full flex-col items-center justify-center">
-                                <p className="text-xl font-semibold text-own-neutral-200 dark:text-own-neutral-100 sm:text-lg md:text-xl xl:text-xl">
-                                  Read more
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="flex flex-col">
-                            <h3 className="text-xl font-semibold text-own-neutral-900 dark:text-own-neutral-200 sm:text-lg md:text-xl xl:text-xl">
-                              {project.name}
-                            </h3>
-                            <p className="text-md font-medium text-own-neutral-400 dark:text-own-neutral-300 sm:text-base md:text-lg xl:text-lg">
-                              {project.tag}
-                            </p>
-                          </div>
-                        </div>
-                      </Link>
-                    </SplideSlide>
-                  )
-                })}
-              </Splide>
+              <CarouselProjects
+                // projects={projectsData.filter(
+                //   project => project.featured === undefined,
+                // )}
+                projects={projectsData}
+              />
             </div>
           </section>
 
