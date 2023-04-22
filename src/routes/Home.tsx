0de55a0377f88815fc3a86a6cf6f69156f271a83
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-04-22 20:35:48 +0200

    feat: footer

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -190,7 +190,19 @@ export default () => {
           </section>
 
           {/* Footer */}
-          <footer className="bg-own-neutral-50 dark:bg-own-neutral-800"></footer>
+          <footer className="bg-own-neutral-50 px-4 pt-[52px] dark:bg-own-neutral-800 md:px-6 lg:pt-[68px]">
+            <div className="mx-auto max-w-screen-xl py-2">
+              <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+                <p className="md:text-1xl transform px-1 text-center text-lg font-black text-own-neutral-600 dark:text-own-neutral-400 lg:text-3xl">
+                  LJ
+                </p>
+                <p className=" text-center text-sm font-medium text-own-neutral-700 dark:text-own-neutral-300 md:text-right md:text-base ">
+                  &copy; {new Date().getFullYear()} Lei Jin - All rights
+                  reserved
+                </p>
+              </div>
+            </div>
+          </footer>
         </main>
       </div>
     </>
