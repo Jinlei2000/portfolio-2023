e74a79e98fcf33b819addaf686d65ea64ca45a68
Lei <jinleix3@hotmail.com>
2024-02-03 13:26:45 +0100

    feat: add descriptiotn 3d car

--- src/routes/projects/Car3D.tsx
+++ src/routes/projects/Car3D.tsx
@@ -7,27 +7,15 @@ export default ({ project }: { project: IProject }) => {
     return (
       <>
         <p>
-          Delving into the realm of mobile app development, my research question
-          sparked an exciting journey: "
-          <span className="font-semibold">
-            How can I create a delectable pizza app with Flutter, enriched by
-            the immersive world of Augmented Reality?
-          </span>
-          "
-        </p>
-        <p>
-          In this Flutter-powered application, I seamlessly integrated the ARKit
-          plugin,{' '}
-          <span className="font-semibold">elevating the user experience</span>,
-          especially tailored for iOS enthusiasts. The app's focal point?
-          Empowering users to customize their pizza by choosing its size and
-          virtually experimenting with various sauces and toppings. This{' '}
-          <span className="font-semibold">
-            dynamic and interactive exploration
-          </span>{' '}
-          allows users to visually savor their selected toppings on the pizza
-          canvas, turning the decision-making process into a delightful, visual
-          feast.
+          Explore a <span className="font-semibold">rapid journey</span> into
+          the world of 3D with project! In just 2 hours, I crafted a{' '}
+          <span className="font-semibold">dynamic experience</span> using
+          Three.js a 3D car that responds to your every command. Control its
+          movement with <span className="font-semibold">WASD</span>, hit the{' '}
+          <span className="font-semibold">space bar</span> to switch background
+          colors, and immerse yourself in a captivating blend of{' '}
+          <span className="font-semibold">creativity</span> and{' '}
+          <span className="font-semibold">technology</span>.
         </p>
       </>
     )
@@ -37,29 +25,9 @@ export default ({ project }: { project: IProject }) => {
     return (
       <>
         <Image
-          link={project.imgs[0].src}
-          alt={project.imgs[0].alt}
-          className="col-span-full lg:col-span-10 lg:col-start-1"
-        />
-        <Image
-          link={project.imgs[1].src}
-          alt={project.imgs[1].alt}
-          className="col-span-full lg:col-span-10 lg:col-start-3"
-        />
-        <Image
-          link={project.imgs[2].src}
-          alt={project.imgs[2].alt}
-          className="col-span-full lg:col-span-10 lg:col-start-1"
-        />
-        <Image
-          link={project.imgs[3].src}
-          alt={project.imgs[3].alt}
-          className="col-span-full lg:col-span-10 lg:col-start-3"
-        />
-        <Image
-          link={project.imgs[4].src}
-          alt={project.imgs[4].alt}
-          className="col-span-full lg:col-span-10 lg:col-start-1"
+          link={project.image}
+          alt={project.name}
+          className="col-span-full lg:col-span-10 lg:col-start-2"
         />
       </>
     )
